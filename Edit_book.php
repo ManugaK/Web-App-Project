6dc5a8dc72270f112878e87973e9da2b9f63d06d
N.H.M.Dilhara <145148074+pro-iceman@users.noreply.github.com>
2024-06-07 15:12:06 +0530

    change logout button

--- Edit_book.php
+++ Edit_book.php
@@ -56,7 +56,7 @@ if (isset($_GET['book_id'])) {
           </div>
 
           <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Log Out</a>
+          <a class="navbar-brand" href="logout.php" style="color:red"><b>Logout</b></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
--- Register_book.php
+++ Register_book.php
@@ -81,7 +81,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           </div>
 
           <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Log Out</a>
+          <a class="navbar-brand" href="logout.php" style="color:red"><b>Logout</b></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
