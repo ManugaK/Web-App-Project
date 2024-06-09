90219ceab81b88f2d4d79da53073ea42d0239397
MadushanLSKK <Kmadushan396@gmail.com>
2024-06-09 13:52:43 +0530

    bootstrap link added

--- Register_category.php
+++ Register_category.php
@@ -159,3 +159,11 @@ mysqli_close($conn);
         <button type="submit" class="btn btn-primary">Register Category</button>
     </form>
 </div>
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
